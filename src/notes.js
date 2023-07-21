ecc4bae50fe2f2f9d4e4bd4146bd35831cc153e4
Mahak Kushwaha <107416996+mahak-23@users.noreply.github.com>
2023-07-21 22:13:00 +0530

    Initial commit

---
+++ src/notes.js
@@ -0,0 +1,28 @@
+const notes = [
+  {
+    key: 1,
+    title: "Delegation",
+    content:
+      "Delegation is simply passing a duty off to someone/something else. Delegation can be an alternative to inheritance."
+  },
+  {
+    key: 2,
+    title: "Loops",
+    content:
+      "Almost all the programming languages provide a concept called loop, which helps in executing one or more statements up to a desired number of times."
+  },
+  {
+    key: 3,
+    title: "Array",
+    content:
+      "An array is a container object that holds a fixed number of values of a single type. The length of an array is established when the array is created."
+  },
+  {
+    key: 4,
+    title: "Hardware vs. Software",
+    content:
+      "Hardware is the physical parts that make up a computer such as a keyboard, and monitor. Software is the programs that run on a computer in order for you to use it."
+  }
+];
+
+export default notes;
